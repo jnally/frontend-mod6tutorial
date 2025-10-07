ca2a52e42301ad0cce117df7c28963954914ede2
Jeremy Nally <jnally3@ivytech.edu>
2025-10-06 23:11:25 -0400

    improve github pages functionality - jn

--- scripts/script.js
+++ scripts/script.js
@@ -4,7 +4,7 @@ addEventListener("DOMContentLoaded", async function(){
     let html = "";
     for(let song of songs){
         let songID = song._id;
-        html += `<li>${song.title} - ${song.artist} - <a href="details.html?id=${songID}">Details</a> - <a href="edit.html?id=${songID}">Edit</a></li>`;
+        html += `<li>${song.title} - ${song.artist} - <a href="./details.html?id=${songID}">Details</a> - <a href="./edit.html?id=${songID}">Edit</a></li>`;
     }
 
     document.querySelector("#list_of_songs").innerHTML = html;
